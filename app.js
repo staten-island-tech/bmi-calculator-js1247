625600f4a6356e44b5d3cd371dd9f6c068e74d09
js1247 <jasonshi1247@gmail.com>
2023-10-05 09:25:04 -0400

    js

--- app.js
+++ app.js
@@ -1,41 +1,26 @@
-// declare and define function, input output machines
-// we can pass in argument or parameters
-function test(mess) {
-    console.log(mess)
+function checkBMI(height, weight){
+    return ((weight/(height*height))*10000);
 }
-//run the function
-test(true);
+const username = prompt("Enter your Name");
+const weight = prompt("Enter your weight in kg");
+const height = prompt("Enter your height in cm");
 
-//string ""
-const x = "string"
-//declaring a variable, defining a variable
-test(x);
-
-//number/integer
-const y =98;
-test(y);
-
-//booleon true or false
-const z = true;
-test(z);
-
-function add(x, y) {
-    console.log(x + y);
+function BMI (checkBMI){
+    if(checkBMI < 18.5){
+        return ("UNDERWEIGHT");
+    }
+    else if(checkBMI >= 18.5 && checkBMI < 25){
+        return ("HEALTHY");
+    }
+    else if(checkBMI >= 25 && checkBMI < 30){
+        return ("OVERWEIGHT");
+    }
+    else {
+        return ("OBESE");
+    }
 }
-//need to convert the string to a number
-let a = number(prompt("enter a number"));
-let b = number(prompt("enter another number"));
-add(a, b);
 
-function add(x,y) {
-    //creates an output
-    return x + y;
-}
-const sum = add(45, 42)
-
-
-function calc() {
-    const sum = add(45, 34);
-    console.log(sum);
-}
-calc();
+const bmi = checkBMI(height, weight)
+console.log(username);
+console.log("BMI= " + bmi);
+console.log(" " + BMI(bmi));
