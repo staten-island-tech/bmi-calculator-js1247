6dbd1ea3fe54a25916550f2b4c91193a80848d91
js1247 <jasonshi1247@gmail.com>
2023-10-06 09:23:47 -0400

    Create onject.js

---
+++ onject.js
@@ -0,0 +1,18 @@
+let nums = [1, 2, 3, 4, 5]
+
+console.log(nums[0]);
+
+nums.foreach((num) => console.log(num));
+
+const students = (    
+{
+    firstname: "Evan", lastname: "Bao"
+},
+{
+    firstname: "Leo", lastname: "Ying"
+},
+{
+    firstname: "Ant", lastname: "Zheng"
+}
+)
+
